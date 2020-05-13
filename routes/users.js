912b6569d8b74f01f28e455f1ecba8fc2c2b3bb7
hieu thai <ngochieu642@hieus-Mac.local>
2020-05-13 22:53:54 +0700

    Format code

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controller/users");
-const userValidate = require('../middleware/users.validate');
+const userValidate = require("../middleware/users.validate");
 
 router.get("/create", controller.getCreate);
 router.get("/:id", controller.getItemById);
@@ -10,4 +10,4 @@ router.get("/:id/delete", controller.getDeleteById);
 router.post("/:id/update", controller.postUpdateById);
 router.post("/create", userValidate.postCreate, controller.postCreate);
 
-module.exports = router
+module.exports = router;
