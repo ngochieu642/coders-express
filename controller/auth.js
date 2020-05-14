fe7464542eea234352f521b0e345c412d90bd0df
ngochieu642 <ngochieu10041997@gmail.com>
2020-05-15 01:29:11 +0700

    Add login form

---
+++ controller/auth.js
@@ -0,0 +1,35 @@
+const db = require("../db");
+const shortId = require("shortid");
+const debugService = require("debug-level").log("auth");
+
+module.exports = {
+  getLogin: function (req, res) {
+    res.render("auth/login");
+  },
+
+  postLogin: function (req, res, next) {
+    let { email, password } = req.body;
+    let user = db.get("users").find({ email: email }).value();
+
+    if (!user) {
+      debugService.warn(`User with email ${email} does not exist`);
+      res.render("auth/login", {
+        errors: ["User does not exist"],
+        values: req.body,
+      });
+      return;
+    }
+
+    if (user.password !== password) {
+      res.render("auth/login", {
+        errors: ["Wrong password"],
+        values: req.body,
+      });
+      return;
+    }
+
+    debugService.info(`UserId ${user.id} logged in`);
+    res.cookie('userId', user.id)
+    res.redirect("/books");
+  },
+};
--- server.js
+++ server.js
@@ -8,10 +8,12 @@ const app = express();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const { countCookie } = require("./middleware/cookie");
+const { requireAuth } = require("./middleware/auth");
 
 const userRoutes = require("./routes/users");
 const bookRoutes = require("./routes/books");
 const transactionRoutes = require("./routes/transactions");
+const authRoutes = require("./routes/auth");
 
 app.set("view engine", "pug");
 app.set("views", "./views");
@@ -20,9 +22,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use("/users", countCookie, userRoutes);
-app.use("/books", countCookie, bookRoutes);
-app.use("/transactions", countCookie, transactionRoutes);
+app.use("/users", countCookie, requireAuth, userRoutes);
+app.use("/books", countCookie, requireAuth, bookRoutes);
+app.use("/transactions", countCookie, requireAuth, transactionRoutes);
+app.use("/auth", countCookie, authRoutes);
 
 // Render
 app.get("/", countCookie, (request, response) => {
